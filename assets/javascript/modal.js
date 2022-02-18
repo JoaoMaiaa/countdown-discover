43b05a7769a7c92d1fd65e8ab5732c40b29e8ed5
João Maia <15joaomaia@gmail.com>
2022-02-18 18:09:07 -0300

    Revert "Revert "Revert "Revert "projeto quase pronto, adicionado o modal, falta apenas o form do modal""""
    This reverts commit 805c23edf299913fe3ab820ddf6b358ffa451105.

---
+++ assets/javascript/modal.js
@@ -0,0 +1,11 @@
+function openModal(e){
+    let modal = document.getElementById('modal')
+    modal.classList.add('is-active')
+    console.log(modal)
+}
+
+function closeModal(e){
+    let modal = document.getElementById('modal')
+    modal.classList.remove('is-active')
+    console.log(modal)
+}
